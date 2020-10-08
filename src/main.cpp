b23b962d0efa82ed6ca541f840dab08c337aa353
Walid Amriou <walidamriou@gmail.com>
2020-10-08 15:58:07 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,28 @@
 
 PCF8591 pcf8591(PCF8591_I2C_ADDRESS);
 float volatge_valure=3.3;
+boolean done = 0;
+
 
 void setup()
 {
 	Serial.begin(115200);
 	pcf8591.begin();
-  pcf8591.voltageWrite(volatge_valure); // 2.7Volts output
+  Serial.setTimeout(1000);
+  //pcf8591.voltageWrite(volatge_valure); // 2.7Volts output
 }
 
 void loop()
 {
-  if (Serial.available() > 0) {
-    volatge_valure = Serial.parseFloat();
-    delay(3000);
+    pcf8591.voltageWrite(volatge_valure); 
+  //if (done == 0){
+  if ( Serial.available() !=0 ){
+     volatge_valure = Serial.parseFloat();
+     //stream.flush()
+     Serial.print(volatge_valure);
+     //done = 1;    //delay(3000);
   }
-  pcf8591.voltageWrite(volatge_valure); // 2.7Volts output
+  //}
 
 }
 
