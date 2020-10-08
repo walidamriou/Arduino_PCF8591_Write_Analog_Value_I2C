87a072d986898c5f8eddf7dee7bb8329a9739648
Walid Amriou <walidamriou@gmail.com>
2020-10-08 18:58:01 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,33 @@
 /*
   Arduino_PCF8591_Write_Analog_Value_I2C
 
-   Developed by: Walid Amriou
-   Last update: October 2020
+  Developed by: Walid Amriou
+  Last update: October 2020
 
  */
 #include "Arduino.h"
-
 #include "PCF8591.h"
+
 #define PCF8591_I2C_ADDRESS 0x48
 
 PCF8591 pcf8591(PCF8591_I2C_ADDRESS);
-float volatge_valure=3.3;
-boolean done = 0;
+float voltage_valure=3.3;
+float buffer_voltge_read;
 
-
-void setup()
-{
+void setup(){
 	Serial.begin(115200);
 	pcf8591.begin();
-  Serial.setTimeout(1000);
-  //pcf8591.voltageWrite(volatge_valure); // 2.7Volts output
 }
 
-void loop()
-{
-    pcf8591.voltageWrite(volatge_valure); 
-  //if (done == 0){
+void loop(){
+  pcf8591.voltageWrite(voltage_valure); 
   if ( Serial.available() !=0 ){
-     volatge_valure = Serial.parseFloat();
-     //stream.flush()
-     Serial.print(volatge_valure);
-     //done = 1;    //delay(3000);
+    buffer_voltge_read = Serial.parseFloat();
+    if(buffer_voltge_read!=0.0){
+      voltage_valure = buffer_voltge_read;
+    }
   }
-  //}
-
+  delay(1);    
 }
 
  
